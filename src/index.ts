8ce88a79f27fecc9e547203423e07e31b3dec468
AnasGherfal <79544599+AnasGherfal@users.noreply.github.com>
2024-02-08 15:49:53 +0200

    testing the routes from frontend

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@ import bodyParser from "body-parser";
 import config from "../config";
 import cors from "cors"
 import {routes} from './routes/routes'
+import path from "path"; 
 const app = express();
 
  app.use(express.json());
  app.use(cors(({ origin:'https://clinic-whnd.onrender.com'})));
  app.use(bodyParser.json());
+ app.use(express.static(path.join(__dirname, "../client/build")));
 
  app.use('/api', routes)
-
+ app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
 app.listen(config.port, () => {
 });
